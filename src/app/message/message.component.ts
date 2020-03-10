195ef1e8962593b3f33d3458fdf3616be9e6f829
heyuall <zoubeidihoussem@gmail.com>
2020-03-10 11:26:33 +0100

    added animation when adding message

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; 
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
+  styleUrls: ['./message.component.scss'],
+  animations: [
+
+    trigger('messages', [
+      transition('* => *', [
+
+        query(':enter', style({ opacity: 0 }), {optional: true}),
+
+        query(':enter', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+          ]))]), {optional: true})
+      ])
+    ])
+
+  ]
 })
 export class MessageComponent {
 
   messageCount: number;
   btnText: string = 'Ajouter';
   messageText: string; 
-  messages = [];
+  messages = ['Bonne Année', 'Bienvenue', 'Bonne Fête'];
 
 
   constructor( private router: Router) {  }
